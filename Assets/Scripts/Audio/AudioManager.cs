751bbed5c98d05139a30dea41c01ffcf72cd8ea0
Luis Montaña <LuisMontana@users.noreply.github.com>
2023-08-31 15:42:45 -0500

    Added audiomanager along with prefab for it

---
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance;
+    [SerializeField] private AudioSource _musicSource, _effectsSource;
+    private void Awake() {
+        if(Instance == null){
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else {
+            Destroy(gameObject);
+        }
+    }
+
+    public void PlayEffect(AudioClip clip){
+        // PlayOneShot wont stop simultaneous audio clips.
+        _effectsSource.PlayOneShot(clip);
+    }
+
+    public void ChangeMusic(AudioClip clip){
+        _musicSource.Stop();
+        _musicSource.clip = clip;
+        _musicSource.Play();
+    }
+
+    public void ChangeMasterVolume(float value) {
+        AudioListener.volume = value;
+    }
+
+}
