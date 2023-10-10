d7b09a333a5befec39b6d07d9c4fbb798028f42c
Ian Cherabier <iancher06@hotmail.fr>
2023-10-10 23:00:49 +0200

    Add camera which zooms out to reveal obstacle
    [WIP - Secret Room]

---
+++ Assets/Scripts/Camera/ChangeCamera.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+
+public class ChangeCamera : MonoBehaviour
+{
+    [SerializeField] CinemachineVirtualCamera targetCamera;
+    private CinemachineVirtualCamera originCamera;
+    private bool isPlayerInZone = false;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!other.CompareTag("Player") || isPlayerInZone) return;
+        isPlayerInZone = true;
+
+        GetCurrentActiveCamera();
+        ActivateTargetCamera();
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (!other.CompareTag("Player") || !isPlayerInZone) return;
+        isPlayerInZone = false;
+
+        ReactivateOriginCamera();
+    }
+
+    private void ActivateTargetCamera()
+    {
+        originCamera.gameObject.SetActive(false);
+        targetCamera.gameObject.SetActive(true);
+    }
+
+    private void ReactivateOriginCamera()
+    {
+        if (originCamera == null)
+        {
+            Debug.Log(
+                "The player exits the special camera zone but there is no origin camera to switch back to. " +
+                "The player probably did not enter the zone beforehand."
+            );
+            return;
+        }
+        
+        originCamera.gameObject.SetActive(true);
+        targetCamera.gameObject.SetActive(false);
+    }
+
+    private void GetCurrentActiveCamera()
+    {
+        CinemachineVirtualCamera[] allVirtualCameras = FindObjectsOfType<CinemachineVirtualCamera>();
+        foreach (CinemachineVirtualCamera virtualCamera in allVirtualCameras)
+        {
+            if (virtualCamera.isActiveAndEnabled)
+            {
+                originCamera = virtualCamera;
+                break;
+            }
+        }
+    }
+}
