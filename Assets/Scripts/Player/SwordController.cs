fc3411804e28b4fd0ec3d2c3821796df40c046cb
Ian Cherabier <iancher06@hotmail.fr>
2023-10-11 23:27:02 +0200

    Prevent player from being pushed when hitting another trigger with sword

--- Assets/Scripts/Player/SwordController.cs
+++ Assets/Scripts/Player/SwordController.cs
@@ -7,6 +7,8 @@ public class SwordController : MonoBehaviour
     [SerializeField] private PlayerController _player;
     private void OnTriggerEnter2D(Collider2D other)
     {
+        if (other.isTrigger) return;
+        
         Debug.Log(other.name);
         Vector2 direction = new Vector2(other.transform.position.x < _player.transform.position.x ? 1 : -1, 0);
         _player.ApplyPushBack(direction);
