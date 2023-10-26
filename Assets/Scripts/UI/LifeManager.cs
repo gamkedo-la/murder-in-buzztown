4197579fb2f8dae164a398cd846b706d01c513f9
Steven Solof <stebssbets@yahoo.com>
2023-10-26 12:14:59 -0400

    player dies if hearts = 0

--- Assets/Scripts/UI/LifeManager.cs
+++ Assets/Scripts/UI/LifeManager.cs
@@ -5,6 +5,12 @@ public class LifeManager : MonoBehaviour
     private int _currentLives = 3;
     private int _maxLives = 3; // Initially this will be 3 but you can increase it to 4
 
+    private GameObject player;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
     public void IncreaseLives()
     {
         if (_currentLives == _maxLives) return;
@@ -14,8 +20,13 @@ public class LifeManager : MonoBehaviour
 
     public void DecreaseLives()
     {
-        if (_currentLives < 1) return;
+        
         transform.GetChild(_currentLives - 1).GetChild(0).gameObject.SetActive(false);
         _currentLives--;
+
+        if (_currentLives < 1)
+        {
+            Destroy(player);
+        }
     }
 }
