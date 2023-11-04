53dc5a9a0ece448faf22364cc90596ead00cac50
Ian Cherabier <iancher06@hotmail.fr>
2023-11-04 10:10:10 +0100

    Add delay before respawn

--- Assets/Scripts/Environment/Void.cs
+++ Assets/Scripts/Environment/Void.cs
@@ -1,16 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class Void : MonoBehaviour
 {
     [SerializeField] private Transform respawnLocation;
+    [SerializeField] private float timeToRespawn = 1f;
     private Transform playerTransform;
+    private CinemachineBrain brain;
     private bool isPlayerFallen = false;
 
     private void Start() 
     {
         playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        brain = FindObjectOfType<CinemachineBrain>();
     }
     
     // Update is called once per frame
@@ -22,9 +26,19 @@ public class Void : MonoBehaviour
         );
     }
 
-    private void OnTriggerEnter2D(Collider2D other) {
-        if (!other.CompareTag("Player")) return;
+    private void OnTriggerEnter2D(Collider2D other) 
+    {
+        if (!other.CompareTag("Player") || isPlayerFallen) return;
+        isPlayerFallen = true;
+        StartCoroutine(RespawnPlayerAfterDelay(other.gameObject));
+    }
+
+    private IEnumerator RespawnPlayerAfterDelay(GameObject other)
+    {
+        brain.ActiveVirtualCamera.Follow = null;
+        yield return new WaitForSeconds(timeToRespawn);
         other.transform.position = respawnLocation.position;
         isPlayerFallen = false;
+        brain.ActiveVirtualCamera.Follow = other.transform;
     }
 }
