4b1a62ddf3c6f7dbb805830513b5af847010369b
Christer Kaitila <breakdance@mcfunkypants.com>
2023-11-14 15:46:13 -0800

    pipes and city skyline buildings added to background

--- Assets/Scripts/Effects/ParallaxBackgroundScroller.cs
+++ Assets/Scripts/Effects/ParallaxBackgroundScroller.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,6 +8,9 @@ public class ParallaxBackgroundScroller : MonoBehaviour
 
     private float length, startpos;
     public float scrollSpeedMultiplier = 0.5f;
+
+    // this is a bit buggy - we just make the sprites really large instead
+    public Boolean loopBasedOnSpriteWidth = false; 
 
 
     void Start()
@@ -20,10 +24,13 @@ public class ParallaxBackgroundScroller : MonoBehaviour
         float temp = (Camera.main.transform.position.x * (1 - scrollSpeedMultiplier));
         float dist = (Camera.main.transform.position.x * scrollSpeedMultiplier);
 
+        // FIXME: this is buggy! backwards from reality, where background moves SLOWER than foreground
         transform.position = new Vector3(startpos + dist, transform.position.y, transform.position.z);
 
-        if (temp > startpos + length) startpos += length;
-        else if (temp < startpos - length) startpos -= length;
+        if (loopBasedOnSpriteWidth) {
+            if (temp > startpos + length) startpos += length;
+            else if (temp < startpos - length) startpos -= length;
+        }
     }
 }
 
