18a2b4ea9f31580479a59e1bcfbcaae56333b7f2
Christer Kaitila <breakdance@mcfunkypants.com>
2023-11-15 11:20:25 -0800

    bugfix parallax scroller script

--- Assets/Scripts/Effects/ParallaxBackgroundScroller.cs
+++ Assets/Scripts/Effects/ParallaxBackgroundScroller.cs
@@ -9,28 +9,17 @@ public class ParallaxBackgroundScroller : MonoBehaviour
     private float length, startpos;
     public float scrollSpeedMultiplier = 0.5f;
 
-    // this is a bit buggy - we just make the sprites really large instead
-    public Boolean loopBasedOnSpriteWidth = false; 
-
-
     void Start()
     {
         startpos = transform.position.x;
-        length = GetComponent<SpriteRenderer>().bounds.size.x;
     }
 
     void Update()
     {
-        float temp = (Camera.main.transform.position.x * (1 - scrollSpeedMultiplier));
-        float dist = (Camera.main.transform.position.x * scrollSpeedMultiplier);
+        float dist = (Camera.main.transform.position.x * (1-scrollSpeedMultiplier));
 
-        // FIXME: this is buggy! backwards from reality, where background moves SLOWER than foreground
         transform.position = new Vector3(startpos + dist, transform.position.y, transform.position.z);
 
-        if (loopBasedOnSpriteWidth) {
-            if (temp > startpos + length) startpos += length;
-            else if (temp < startpos - length) startpos -= length;
-        }
     }
 }
 
