2b2f33df47138bdd3b06dab17de7cdba3dc3e3a3
Luis Montaña <lmontana@live.com>
2023-11-23 18:26:48 -0500

    added dev util to teleport to boss room

--- Assets/Scripts/Utils/DevUtils.cs
+++ Assets/Scripts/Utils/DevUtils.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class DevUtils : MonoBehaviour
 {
     [SerializeField] GameObject _bossCam;
     [SerializeField] GameObject _followCam;
+    [SerializeField] GameObject _player;
+    [SerializeField] TextMeshProUGUI tmproComponent;
+    private bool isInBossRoom = false;
 
     public void SwitchToBossCam()
     {
@@ -17,5 +21,21 @@ public class DevUtils : MonoBehaviour
     {
         _bossCam.SetActive(false);
         _followCam.SetActive(true);
+    }
+
+    public void TeleportToBossRoom()
+    {
+        if (isInBossRoom)
+        {
+            _player.transform.position = new Vector3(-14f, 2, 0f);
+            tmproComponent.text = "TP to Boss";
+            SwitchToFollowCam();
+        }
+        else
+        {
+            _player.transform.position = new Vector3(-338f, 2, 0f);
+            tmproComponent.text = "TP to Start";
+            SwitchToBossCam();
+        }
     }
 }
