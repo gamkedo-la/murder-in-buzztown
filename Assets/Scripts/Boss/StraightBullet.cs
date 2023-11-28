fee6be29250d4b59d6ce2a749d7986d41b311219
Luis Montaña <lmontana@live.com>
2023-11-28 16:13:40 -0500

    Bullets now damage you and are destroyed when they hit any boundaries

--- Assets/Scripts/Boss/StraightBullet.cs
+++ Assets/Scripts/Boss/StraightBullet.cs
@@ -12,5 +12,23 @@ public class StraightBullet : MonoBehaviour
         _target = pos;
         Vector2 direction = _target - transform.position;
         GetComponent<Rigidbody2D>().velocity = direction.normalized * SPEED;
+        Invoke("DestroySelf", 8f); // in case it doesn't collide with anything just kill it
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Boss")) return;
+        if (other.CompareTag("Player"))
+        {
+            GameManager.Instance.DecreaseLives();
+            Vector2 direction = new Vector2(transform.position.x < other.transform.position.x ? 1 : -1, 0);
+            other.GetComponent<PlayerController>().ApplyPushBack(direction, true);
+        }
+        DestroySelf();
+    }
+
+    void DestroySelf()
+    {
+        Destroy(gameObject);
     }
 }
