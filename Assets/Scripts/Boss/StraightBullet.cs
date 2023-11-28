f26a56b055414bf78c9ff60d3b744331403e180c
Luis Montaña <lmontana@live.com>
2023-11-28 15:50:31 -0500

    bullets now are shot in a straight line instead of just stopping at the point that they had as target

--- Assets/Scripts/Boss/StraightBullet.cs
+++ Assets/Scripts/Boss/StraightBullet.cs
@@ -5,19 +5,12 @@ using UnityEngine;
 public class StraightBullet : MonoBehaviour
 {
     private Vector3 _target;
-    const float SPEED = 5f;
+    const float SPEED = 10f;
 
     public void SetTarget(Vector3 pos)
     {
         _target = pos;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (_target != Vector3.zero)
-        {
-            transform.position = Vector3.MoveTowards(transform.position, _target, SPEED * Time.deltaTime);
-        }
+        Vector2 direction = _target - transform.position;
+        GetComponent<Rigidbody2D>().velocity = direction.normalized * SPEED;
     }
 }
