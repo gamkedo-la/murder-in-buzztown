1c0a37183e69b0b9a2618a433e674f657e27dd74
Luis Montaña <lmontana@live.com>
2023-12-10 11:35:22 -0500

    Added credits to main menu

--- Assets/Scripts/UI/MenuController.cs
+++ Assets/Scripts/UI/MenuController.cs
@@ -5,24 +5,36 @@ using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour
 {
+    [SerializeField] GameObject _creditsPanel;
+    bool _isPanelActive = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape) && _isPanelActive)
+        {
+            _creditsPanel.SetActive(false);
+            _isPanelActive = false;
+        }
     }
 
-    public void StartGame(){
+    public void StartGame()
+    {
         SceneManager.LoadSceneAsync(1);
 
     }
 
-    public void LoadOptions(){
-        Debug.Log("Not Currently Implemented");
+    public void LoadOptions()
+    {
+        if (!_isPanelActive)
+        {
+            _creditsPanel.SetActive(true);
+            _isPanelActive = true;
+        }
     }
 }
